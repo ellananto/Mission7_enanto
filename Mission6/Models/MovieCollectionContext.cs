a199bc5f3f35f54e469fea15ad2ecb0c6cfe1096
ellananto <ellananto9@gmail.com>
2023-02-08 11:40:46 -0700

    new changes

---
+++ Mission6/Models/MovieCollectionContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Models
+{
+    public class MovieCollectionContext : DbContext
+    {
+        public MovieCollectionContext (DbContextOptions<MovieCollectionContext> options) : base (options)
+        {
+            // leave blank for now
+        }
+        public DbSet<MovieFormResponse> responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<MovieFormResponse>().HasData(
+               new MovieFormResponse
+               {
+                   RentalID = 1,
+                   Category = "Suspense", 
+                   Title = "Inception",
+                   Year = "2010",
+                   Director = "Christopher Nolan",
+                   Rating = "PG-13",
+                   Edited = false,
+                   Notes = "My favorite movie!"
+               },
+               new MovieFormResponse
+               {
+                   RentalID = 2,
+                   Category = "Action",
+                   Title = "Age of Ultron",
+                   Year = "2015",
+                   Director = "Kevin Feige",
+                   Rating = "PG-13",
+                   Edited = false,
+                   Notes = "Avengers are the best!"
+               },
+               new MovieFormResponse
+               {
+                   RentalID = 3,
+                   Category = "Musical",
+                   Title = "High School Musical 3: Senior Year",
+                   Year = "2008",
+                   Director = "Disney",
+                   Rating = "PG",
+                   Edited = false,
+                   Notes = "The best ending!"
+               }
+           );
+        }
+    }
+}
---
+++ Mission6/Models/MovieFormResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Models
+{
+    public class MovieFormResponse
+    {
+        [Key]
+        [Required]
+        public int RentalID { get; set; }
+        public string Category { get; set; }
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Director { get; set; }
+        public string Rating { get; set; }
+        public bool Edited { get; set; }
+        public string LentTo { get; set; }
+        [StringLength(25, MinimumLength = 0)]
+        public string Notes { get; set; }
+    }
+}
